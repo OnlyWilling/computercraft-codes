997054454fc7c35e3d04e4f5faa12b30dcb4a18d
OnlyWilling <onlywilling2020@gmail.com>
2025-11-28 18:34:18 +0800

    Feat: Adjust bimg imae size

--- Take Six/test.lua
+++ Take Six/test.lua
@@ -1,17 +1,30 @@
 local basalt = require("basalt")
 
+local function loadImageFile(path)
+    local file, err = fs.open(shell.resolve(path), "rb")
+    if not file then error("Cannot open file: " .. err) end
+    local data = file.readAll()
+    file.close()
+    local success, img = pcall(textutils.unserialize, data)
+    if not success then error("Invalid BIMG file: " .. img) end
+    return img
+end
+
 term.clear()
 term.setCursorPos(1, 1)
 
-local video_path = "nimmt.bimg"
+local bimg_path = "./bimg/nimmt2.bimg"
+local bimg_data = loadImageFile(bimg_path)
 
 local main = basalt.createFrame()
+    :setSize(51, 19)
+    :setBackground(colors.black)
 
 local img1 = main:addImage({
-        bimg = video_path,
-        autoResize = true,
+        bimg = bimg_data,
     })
-    :setPosition(2, 2)
+    :setPosition(1, 1)
+    :setSize(51, 19)
     :onClick(function()
         basalt.stop()
     end)
